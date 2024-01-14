7ee26b54b63d9f9f4ebe97a2334a076557d0627c
Rana Ahammed <98573219+rana-ahammed@users.noreply.github.com>
2024-01-14 18:01:55 +0600

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,7 @@ const Home = () => {
 				{/* <div className="flex flex-col items-center w-full mt-6 h-auto gap-4"> */}
 				<a
 					className="bg-sky-400 hover:bg-sky-500 text-black p-4 rounded-lg text-xl font-bold mx-auto text-center"
-					href="https://drive.google.com/file/d/1hFO6rv3oj3GXt_JO6M4iSEAYb1Krq7en/view?usp=sharing"
+					href="https://drive.google.com/file/d/1Cab1W39wxuxoUR0vSeyJCjjxeC-okBtN/view?usp=sharing"
 					target="_blank"
 					rel="noreferrer"
 				>
